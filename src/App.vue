3dffa12c86340b82fdf0e76ebc42075ba8ce232f
Mariya Mynkina <mblnkinakk@gmail.com>
2022-07-04 13:37:24 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <el-tabs v-model="activeName">
+      <el-tab-pane label="Users" name="first"
+        ><TabUsers :todos-data="todosData"
+      /></el-tab-pane>
+      <el-tab-pane label="Table" name="second"
+        ><TabTable :todos-data="todosData"
+      /></el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import TabUsers from "./components/TabUsers";
+import TabTable from "./components/TabTable";
+import axios from "axios";
+
+export default {
+  /**
+   * Name.
+   */
+  name: "App",
+
+  /**
+   * Components
+   */
+  components: {
+    TabUsers,
+    TabTable,
+  },
+
+  /**
+   * Mounted hook.
+   */
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/")
+      .then((response) => (this.todosData = response.data));
+  },
+
+  /**
+   * Reactive properties.
+   * @returns {{}}
+   */
+  data: function () {
+    return {
+      todosData: {},
+      activeName: "first",
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import App from "./App";
+
+import ElementUI from "element-ui";
+import locale from "element-ui/lib/locale/lang/en";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI, { locale });
+
+new Vue({
+  el: "#app",
+  render: (h) => h(App)
+});
